5b39210734da92933083b7e9ecef7af4a2e18fdb
Miguel Rabasco <mrabber@etsinf.upv.es>
2026-05-20 23:10:31 +0200

    finished working on the newActivity window and implementing validation for newAnnotation

--- src/app/MapaDemoAppMiguel.java
+++ src/app/MapaDemoAppMiguel.java
@@ -11,17 +11,22 @@ public class MapaDemoAppMiguel extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/newAnnotation.fxml"));
-//        Parent root = FXMLLoader.load(getClass().getResource("/fxml/NewActivity.fxml"));
+        int window = 1; // 0 for New Activity, 1 for new annotation and 2 for cumulative statistics
+        Parent root;
+        
+        // Cases to "dynamically" add the required FXML for testing purposes
+        if(window == 0) root = FXMLLoader.load(getClass().getResource("/fxml/NewActivity.fxml"));
+        else if(window == 1) root = FXMLLoader.load(getClass().getResource("/fxml/NewAnnotation.fxml"));
+        else root = FXMLLoader.load(getClass().getResource("/fxml/CumulativeStatistics.fxml"));
+        
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/resources/logo.png")));
         Scene scene = new Scene(root);
-//        String css = this.getClass().getResource("/css/newActivityStyles.css").toExternalForm();
-//        scene.getStylesheets().add(this.getClass().getResource("/css/statisticsStyles.css").toExternalForm());
-        scene.getStylesheets().add(
-            getClass().getResource("/css/newAnnotationStyles.css").toExternalForm()
-        );
-//        System.out.println(css);
-//        scene.getStylesheets().add(css);
+        
+        // Cases to "dynamically" add the required CSS for testing purposes
+        if(window == 0) scene.getStylesheets().add(this.getClass().getResource("/css/newActivityStyles.css").toExternalForm());
+        else if (window == 1) scene.getStylesheets().add(this.getClass().getResource("/css/newAnnotationStyles.css").toExternalForm());
+        else if (window == 2) scene.getStylesheets().add(this.getClass().getResource("/css/statisticsStyles.css").toExternalForm());
+        
         stage.setTitle("Demo mapas - IPC");
         stage.setScene(scene);
         stage.show();
